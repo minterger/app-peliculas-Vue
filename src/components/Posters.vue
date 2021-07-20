3f7b48bacf0a50e05adf17c9ea9fea0a20abe5b9
minterger <minterger@gmail.com>
2021-07-20 03:24:10 -0300

    update key

--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-4 row row-cols-2 row-cols-sm-3 row-cols-lg-4 row-cols-xl-5 row-cols-xl-6 g-4">
-    <div class="col" v-for="poster in posters" :key="poster.i">
+    <div class="col" v-for="poster in posters" :key="poster.poster_link">
       <router-link class="card mb-2" :to="poster.poster_link">
         <img v-lazy="poster.poster.src" :alt="poster.poster.alt" class="card-img-top">
         <div class="card-body">
