72e84bd2222863057be4afaa40ddb13a9073eb73
minterger <minterger@gmail.com>
2021-07-31 23:13:21 -0300

    fix and optimize carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,17 +1,19 @@
 <template>
   <vueper-slides
     class="no-shadow"
+    :touchable="false"
     :visible-slides="4"
     :arrows-outside="true"
     slide-multiple
     :gap="2"
     :slide-ratio="1 / 7"
     :dragging-distance="200"
-    :breakpoints="{ 1200: { arrowsOutside: false }, 992: { arrowsOutside: false, visibleSlides: 3, slideMultiple: 3, slideRatio: 1 / 4 }, 576: { arrowsOutside: false, visibleSlides: 2, slideMultiple: 2, slideRatio: 1 / 3 } }">
-    <vueper-slide 
-      v-for="poster in posterLast" 
+    :breakpoints="optionBreakPoint"
+  >
+    <vueper-slide
+      v-for="poster in posterLast"
       :key="poster.i"
-      :title="poster.title" 
+      :title="poster.title"
       :image="poster.poster"
       @click.prevent="$router.push(poster.poster_link)"
       :link="poster.poster_link"
@@ -26,6 +28,27 @@ import 'vueperslides/dist/vueperslides.css'
 
 
 export default {
+  data() {
+    return {
+      optionBreakPoint: {
+        1200: {
+          arrowsOutside: false
+        },
+        992: {
+          arrowsOutside: false,
+          visibleSlides: 3,
+          slideMultiple: 3,
+          slideRatio: 1 / 4
+        },
+        576: {
+          arrowsOutside: false,
+          visibleSlides: 2,
+          slideMultiple: 2,
+          slideRatio: 1 / 3
+        }
+      }
+    }
+  },
   components: {
     VueperSlides, VueperSlide
   },
