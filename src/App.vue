2a9febf625384ae42c2ff8e6cb179eea82210dc6
minterger <minterger@gmail.com>
2021-08-20 21:35:26 -0300

    update scroll

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <template>
   <div id="app">
     <Navbar />
-    <router-view/>
+    <router-view />
     <Footer class="footer" />
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Navbar,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style>
-  body {
-    min-height: 100vh;
-    position: relative;
-    padding: 0 0 3em 0;
-    margin: 0;
-  }
-  
-  *::-webkit-scrollbar {
-    width: 12px;
-    background-color: #151a21;
-  }
+body {
+  min-height: 100vh;
+  position: relative;
+  padding: 0 0 3em 0;
+  margin: 0;
+}
 
-  *::-webkit-scrollbar-thumb {
-    background-color: #5d616a;
-    border-radius: 50px;
-    border: 3px solid #151a21;
-  }
-  *::-webkit-scrollbar-thumb:hover {
-    background-color: #4e535e
-  }
-  *::-webkit-scrollbar-thumb:active {
-    background-color: #435ac0;
-  }
-
+/*Scroll*/
+*::-webkit-scrollbar {
+  width: 12px;
+  height: 12px;
+  background-color: #151a21;
+}
+*::-webkit-scrollbar-corner {
+  background-color: #151a21;
+}
+*::-webkit-scrollbar-thumb {
+  background-color: #5d616a;
+  border-radius: 50px;
+  border: 3px solid #151a21;
+}
+*::-webkit-scrollbar-thumb:hover {
+  background-color: #4e535e;
+}
+*::-webkit-scrollbar-thumb:active {
+  background-color: #435ac0;
+}
+/*fin Scroll*/
 </style>
