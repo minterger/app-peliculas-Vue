e2218c5cbc48e9417a21c24091191b4e1c0cdbea
minterger <minterger@gmail.com>
2021-09-15 16:24:39 -0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="navbar navbar-dark bg-dark">
     <div class="container-md">
-      <div class="text-white"><i class='bx bx-copyright'></i> PelisLatino 2021</div>
+      <div class="text-white"><i class='bx bx-copyright'></i> PelisLatino - 2021</div>
+      <!-- <div class="text-white redes">
+        Redes:
+        <a class="social" href="https://facebook.com/pelislatino.ga" target="_blank" rel="noopener noreferrer"><i class='bx bxl-facebook-square'></i></a>
+      </div> -->
     </div>
   </div>
 </template>
@@ -14,5 +18,15 @@
     margin: 0px;
     width: 100%;
     height: 3em;
+  }
+
+  .redes {
+    line-height: 1.5em;
+  }
+
+  .social .bx{
+    color: #3b5998;
+    padding: 3px 0px 0px 5px;
+    transform: scale(2);
   }
 </style>
